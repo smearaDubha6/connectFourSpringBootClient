61e9b77fa9977ccb789e55974710f145b79f7c7c
Richie Duggan <richie_dug@yahoo.com>
2020-03-03 23:22:16 +0000

    Update ConnectClient.java

--- src/main/java/connectFourClient/ConnectClient.java
+++ src/main/java/connectFourClient/ConnectClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.client.RestTemplate;
  * @author Richie Duggan
  */
 public class ConnectClient {
-
+    
     private static final String restService = "http://localhost:8080";
     private static final String acceptingPlayers = restService + "/isAcceptingClients";
     private static final String colourChosen = restService + "/colourChosen";
@@ -22,140 +22,133 @@ public class ConnectClient {
       
     public static void main(String[] args) {
 
-    	RestTemplate restTemplate = new RestTemplate();    	
+    RestTemplate restTemplate = new RestTemplate();    	
     	
-        String colourChosenUser = "";
-        int colourChosenInt = 0;
-        int colourIndex = 0;
+    String colourChosenUser = "";
+    int colourChosenInt = 0;
+    int colourIndex = 0;
     	
-        // see if the server is accepting players
-		ServerAnswer servAns = restTemplate.getForObject(acceptingPlayers, ServerAnswer.class);   
+     // see if the server is accepting players
+     ServerAnswer servAns = restTemplate.getForObject(acceptingPlayers, ServerAnswer.class);   
     	
-		if (!servAns.getAnswer()) {
-			System.out.println("The server is currently not accepting any players.");
-			return;
-		}
+     if (!servAns.getAnswer()) {
+        System.out.println("The server is currently not accepting any players.");
+        return;
+     }
+	
+     System.out.print("Welcome to the game! Please enter your name : ");
 		
-		System.out.print("Welcome to the game! Please enter your name : ");
-		
-		// get player name and register the player to the game
-        String username = "";
+     // get player name and register the player to the game
+     String username = "";
        
-        InputStreamReader streamReader = new InputStreamReader(System.in);
-        BufferedReader bufferedReader = new BufferedReader(streamReader);
+     InputStreamReader streamReader = new InputStreamReader(System.in);
+     BufferedReader bufferedReader = new BufferedReader(streamReader);
         
-        try {
-        	username = bufferedReader.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
+     try {
+        username = bufferedReader.readLine();
+     } catch (IOException e) {
+        e.printStackTrace();
+        return;
+     }
         
-        System.out.println("Thank you " + username);
+     System.out.println("Thank you " + username);
     	
-		Player player = restTemplate.getForObject(addPlayer + "/" + username, Player.class);
+     Player player = restTemplate.getForObject(addPlayer + "/" + username, Player.class);
         
-		if (player == null) {
-			System.out.println("There was a problem registering you with the server");
-			return;
-		}
+     if (player == null) {
+        System.out.println("There was a problem registering you with the server");
+	return;
+     }
 		
-    	// see if colour has been chosen. If not ask the player what colour
-		// is wanted and register this with the server
+    // see if colour has been chosen. If not ask the player what colour
+    // is wanted and register this with the server
 
-		servAns = restTemplate.getForObject(colourChosen, ServerAnswer.class);
+    servAns = restTemplate.getForObject(colourChosen, ServerAnswer.class);
     	
-		if (!servAns.getAnswer()) {
-    	
-			while (colourIndex == 0) {			
-				System.out.print(BoardState.welcomeMessage2());
-				
-				try {
-					colourChosenUser = bufferedReader.readLine();
-			        } catch (IOException e) {
-			            e.printStackTrace();
-			            return;
-			    }
+    if (!servAns.getAnswer()) {	
+	while (colourIndex == 0) {			
+	   System.out.print(BoardState.welcomeMessage2());	
+	      try {
+                 colourChosenUser = bufferedReader.readLine();
+	      } catch (IOException e) {
+	         e.printStackTrace();
+	         return;
+              }
 
-				try {
-					colourChosenInt = Integer.parseInt(colourChosenUser);
+	      try {
+	         colourChosenInt = Integer.parseInt(colourChosenUser);
                  
-					if (colourChosenInt == 1  || colourChosenInt == 2) {
-						colourIndex = colourChosenInt;
-					}
-				} catch (NumberFormatException exception) {
-				}
-			}
+	         if (colourChosenInt == 1  || colourChosenInt == 2) {
+		    colourIndex = colourChosenInt;
+		 }
+	      } catch (NumberFormatException exception) {
+	      }
+	   }
 			
-			player = restTemplate.getForObject(chooseColour + "/" + player.getId() + "/" + colourIndex, Player.class);
+	   player = restTemplate.getForObject(chooseColour + "/" + player.getId() + "/" + colourIndex, Player.class);
 
-			if (player.getColour() == colourIndex) {
-				System.out.println("You have been assigned your preferred colour");
-			} else {
-				System.out.println("Your preferred colour could not be chosen and has been automatically chosen");
-			}
-			
-		}
+	   if (player.getColour() == colourIndex) {
+	      System.out.println("You have been assigned your preferred colour");
+	   } else {
+	      System.out.println("Your preferred colour could not be chosen and has been automatically chosen");
+	   }		
+	}
 		
-		BoardState currentState = new BoardState();
+        BoardState currentState = new BoardState();
 		
-		String colChoiceStr = "";
-		int colChoice = 0;
+        String colChoiceStr = "";
+        int colChoice = 0;
 		
-		// poll the state of the game until it is this player's turn
-		while (!currentState.getGameOver()) {
-			currentState = restTemplate.getForObject(gameState,BoardState.class);	
+	// poll the state of the game until it is this player's turn
+	while (!currentState.getGameOver()) {
+	   currentState = restTemplate.getForObject(gameState,BoardState.class);	
+	      if (currentState.getGameOver()) {
+	         break;
+	      }
 			
-			if (currentState.getGameOver()) {
-				break;
-			}
-			
-			if (currentState.getNumPlayers() != 2 ||
-					currentState.getPlayerGo() != player.getId()) {			
-				try {
-					// TODO : make configurable
-					Thread.sleep(5000);
-				} catch (InterruptedException e) {
-					throw new RuntimeException("Unexpected interrupt", e);
-				}
-				continue;
-			} else {			
-				colChoice = 0;
-				
-				while (colChoice <= 0 || colChoice > BoardState.NUM_COLS ) {
-						
-					System.out.print(currentState.boardMessage(player));
+	      if (currentState.getNumPlayers() != 2 ||
+	            currentState.getPlayerGo() != player.getId()) {			
+	         try {
+		    // TODO : make configurable
+		    Thread.sleep(5000);
+	         } catch (InterruptedException e) {
+	            throw new RuntimeException("Unexpected interrupt", e);
+		 }
+		    continue;
+		 } else {			
+		    colChoice = 0;
+		
+		    while (colChoice <= 0 || colChoice > BoardState.NUM_COLS ) {
+		       System.out.print(currentState.boardMessage(player));
 					
-					try {
-						colChoiceStr = bufferedReader.readLine();
-				        } catch (IOException e) {
-				            e.printStackTrace();
-				            return;
-				    }
+		       try {
+		          colChoiceStr = bufferedReader.readLine();
+		       } catch (IOException e) {
+		          e.printStackTrace();
+		          return;
+		       }
 
-					try {
-						colChoice = Integer.parseInt(colChoiceStr);
-					} catch (NumberFormatException exception) {
-					}
-				}
+		       try {
+		          colChoice = Integer.parseInt(colChoiceStr);
+		       } catch (NumberFormatException exception) {
+		    }
+	         }
 				
-				// send user choice to server
-				currentState = restTemplate.getForObject(takeTurn + "/" + player.getId() + "/" + colChoice,BoardState.class);
-			}
-		}
+	         // send user choice to server
+		 currentState = restTemplate.getForObject(takeTurn + "/" + player.getId() + "/" + colChoice,BoardState.class);
+	      }
+	   }
 		
-		StringBuilder resultText = new StringBuilder();
+	   StringBuilder resultText = new StringBuilder();
 		
-		if (currentState.getWinningColour() == player.getColour()) {
-			resultText.append("You are the winner " + player.getName() + "!!");
-		} else if (currentState.getWinningName() == "") {
-			System.out.println("The server determined too much time has been taken for goes so the game is over");
-		} else {
-			resultText.append("The game is over, " + currentState.getWinningName() + " won. " +
-	                "Thank you for playing.");
-		}
-		
-		System.out.println(resultText.toString() + " Final board is : \n" + currentState.getBoardState());
-			
-    }         
-}
+           if (currentState.getWinningColour() == player.getColour()) {
+	      resultText.append("You are the winner " + player.getName() + "!!");
+	    } else if (currentState.getWinningName() == "") {
+	       System.out.println("The server determined too much time has been taken for goes so the game is over");
+	    } else {
+	       resultText.append("The game is over, " + currentState.getWinningName() + " won. " +
+	             "Thank you for playing.");
+	    }
+	    System.out.println(resultText.toString() + " Final board is : \n" + currentState.getBoardState());
+	 }
+      }
